b427fe5b17ae2a7ea1545b7c1659abb9241cbd00
Natalie Koch <natalie.koch927@gmail.com>
2019-06-10 19:16:46 -0500

    Remove unused dependencies and make general improvements

--- scripts/main.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-
-  $(window).scroll(function(){
-    $(".mouse-wrapper img").css("opacity", 1 - $(window).scrollTop() / 500);
-    $(".mouse-wrapper div p").css("opacity", 1 - $(window).scrollTop() / 500);
-
-  });
-});
